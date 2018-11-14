bdf5cae1459066097820b31f148bd09634bdb9d0
Rostyk Alfavitskiy <wowp21042001@gmail.com>
2018-11-14 09:48:44 +0200

    ended projaects

---
+++ recursion_fibonaccinum.java
@@ -0,0 +1,19 @@
+﻿public class MyClass {
+    public static int fibonacciNum(int n){
+        if (n == 0){
+            return 0;
+        }
+        else if (n == 1 || n == 2){
+            return 1;
+        }
+        else {
+            return fibonacciNum(n-1) + fibonacciNum(n-2);
+        }
+    }
+    public static void main(String args[]) {
+        for (int i = 0; i <= 20; i++){
+        System.out.print(fibonacciNum(i) + " ");
+        }
+    }
+    
+}
---
+++ simple_numbers.java
@@ -0,0 +1,16 @@
+﻿public class MyClass {
+    public static void main(String args[]) {
+        int num, dil; //num - число яке розглядаємо; dil - кільк дільників 
+        for (num = 1; num <= 199; num++){
+            dil = 0;
+            for (int i = 1; i <= num; i++){
+                if (num % i == 0){
+                    dil++;
+                }  
+            }
+            if (dil == 2){ //оскільки просте число ділиться на 1 і на самого себе, тому умова про 2 дільника
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
